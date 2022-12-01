a2bc5c4da5a3f5f93e1e6789c6364c11b9137fd3
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-11-30 22:58:36 -0500

    Task - decrement and increment By 2 solved

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -13,9 +13,14 @@ export const counterSlice = createSlice({
       // immutable state based off those changes
       state.counter += 1
     },
-    
+    decrement: (state) => {
+      state.counter -=1
+    },
+    incrementBy2: (state) => {
+      state.counter +=2
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementBy2 } = counterSlice.actions;
